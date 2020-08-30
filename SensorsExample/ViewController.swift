8ee28124cc5577a076777dc501a616c6098afe1b
José Henrique Fernandes Silva <jhfs@cin.ufpe.br>
2020-08-30 16:45:33 -0300

    Adiciona projeto do Xcode

---
+++ SensorsExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SensorsExample
+//
+//  Created by José Henrique Fernandes Silva on 30/08/20.
+//  Copyright © 2020 José Henrique Fernandes Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
